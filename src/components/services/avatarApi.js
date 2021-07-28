3964f8b6cc24d3dd32b58883087b662887458216
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-27 19:54:16 -0700

    added API fetch

---
+++ src/components/services/avatarApi.js
@@ -0,0 +1,7 @@
+export const fetchCharacters = async () => {
+  const res = await fetch(
+    'https://last-airbender-api.herokuapp.com/api/v1/characters'
+  );
+  const json = await res.json;
+  return json.results;
+};
