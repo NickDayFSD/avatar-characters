fda01dbd6be08ef95fe02f30a2f09adbb8358ec7
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-28 11:24:46 -0700

    installed/setup router dom

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,26 @@
 import React from 'react';
 import AvatarCharacters from '../containers/AvatarCharacters';
+import AvatarIndividual from '../characters/CharacterDetails';
+import { 
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 export default function App() {
-  return <AvatarCharacters />;
+  return (
+    <Router>
+      <Switch>
+
+        <Route path="/">
+          <AvatarCharacters />
+        </Route>
+
+        <Route path="/character/:id">
+          <AvatarIndividual />
+        </Route>
+
+      </Switch>
+    </Router>
+  );
 }
