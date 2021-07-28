c834c670ddc1b5bf1a7475dd7810e662037da0bc
NickDayFSD <Nick.Day.FSD@gmail.com>
2021-07-27 17:48:02 -0700

    List created

---
+++ src/components/characters/CharacterList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Character from './Character';
+
+const CharacterList = ({ characters }) => {
+  const characterElements = characters.map((character) => (
+    <li key={character.id}>
+      <Character
+        {...character}
+      />
+    </li>
+  ));
+  return <ul>{characterElements}</ul>;
+};
+
+CharacterList.PropTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  photoUrl: PropTypes.string.isRequired,
+  affiliation: PropTypes.string.isRequired
+};
+
+export default CharacterList;
